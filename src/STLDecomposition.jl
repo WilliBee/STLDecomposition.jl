d785e702ffe0de12d84844f1f004d42d96453500
william-beng <=>
2023-07-24 13:24:33 +0200

    Initial commit

--- src/STLDecomposition.jl
+++ src/STLDecomposition.jl
@@ -1,5 +1,48 @@
 module STLDecomposition
 
-# Write your package code here.
+using stldecomposition_jll
+
+export call_stl, call_stlez
+
+# Define the Julia function to call the Fortran subroutine
+function call_stl(
+    y::Vector{Float64}, n::Int, np::Int, ns::Int, nt::Int, nl::Int, 
+    isdeg::Int, itdeg::Int, ildeg::Int, 
+    nsjump::Int, ntjump::Int, nljump::Int, ni::Int, no::Int)
+
+    # Initialize arrays
+    rw = similar(y)
+    season = similar(y)
+    trend = similar(y)
+    work = Vector{Float64}(undef, (n+2*np)*7)
+
+    @ccall libstldecomposition.stl_(
+        y::Ref{Float64}, n::Ref{Int64}, np::Ref{Int64}, ns::Ref{Int64}, nt::Ref{Int64}, nl::Ref{Int64}, 
+        isdeg::Ref{Int64}, itdeg::Ref{Int64}, ildeg::Ref{Int64}, 
+        nsjump::Ref{Int64}, ntjump::Ref{Int64}, nljump::Ref{Int64}, ni::Ref{Int64}, no::Ref{Int64}, 
+        rw::Ref{Float64}, season::Ref{Float64}, trend::Ref{Float64}, work::Ref{Float64}
+    )::Cvoid
+
+    return season, trend
+end
+
+
+function call_stlez(y::Vector{Float64}, n::Int, np::Int, ns::Int,isdeg::Int, itdeg::Int, robust::Bool)
+
+    # Initialize counter and arrays
+    nconv = 0
+    rw = similar(y)
+    season = similar(y)
+    trend = similar(y)
+    work = Vector{Float64}(undef, (n+2*np)*7)
+
+    @ccall libstldecomposition.stlez_(
+        y::Ref{Float64}, n::Ref{Int64}, np::Ref{Int64}, ns::Ref{Int64},
+        isdeg::Ref{Int64}, itdeg::Ref{Int64}, robust::Ref{Bool}, nconv::Ref{Int64}, 
+        rw::Ref{Float64}, season::Ref{Float64}, trend::Ref{Float64}, work::Ref{Float64}
+    )::Cvoid
+
+    return season, trend
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,43 @@ using STLDecomposition
 using Test
 
 @testset "STLDecomposition.jl" begin
-    # Write your tests here.
+    file = open("co2.txt")
+    lines = readlines(file)
+    
+    # CO2 data from original implementation https://www.netlib.org/a/stl
+    y = parse.(Float64, split(lines[1]))
+    
+    # Expected results from Fortran with function stl
+    season_stl_fortran = parse.(Float64, split(lines[2]))
+    trend_stl_fortran = parse.(Float64, split(lines[3]))
+    
+    # Expected results from Fortran with function stlez
+    season_stlez_fortran = parse.(Float64, split(lines[5]))
+    trend_stlez_fortran = parse.(Float64, split(lines[6]))
+
+    # Example usage:
+    robust = false
+    n=348
+    np=12
+    ns=35
+    nt=19
+    nl=13
+    no=2
+    ni=1
+    nsjump=4
+    ntjump=2
+    nljump=2
+    isdeg=1
+    itdeg=1
+    ildeg=1
+
+    season_stl_julia, trend_stl_julia = call_stl(y, n, np, ns, nt, nl, isdeg, itdeg, ildeg, nsjump, ntjump, nljump, ni, no)
+
+    @test season_stl_julia == season_stl_fortran
+    @test trend_stl_julia  == trend_stl_fortran
+
+    season_stlez_julia, trend_stlez_julia = call_stlez(y, n, np, ns, isdeg, itdeg, true)
+
+    @test season_stlez_julia == season_stlez_fortran
+    @test trend_stlez_julia  == trend_stlez_fortran
 end
